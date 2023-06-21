3aaf385b9d8cfdaaa23fde7f483713bba72a5d66
Полина Куксова <pakuksova@gmail.com>
2023-06-22 00:40:41 +0300

    change img path

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { Footer } from "./components/footer/Footer";
+import { Footer } from "./components/Footer/Footer";
 import { AppRouter } from "./components/router/AppRouter";
-import { Header } from "./components/header/Header";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
--- src/components/data/contact.ts
+++ src/components/data/contact.ts
@@ -7,22 +7,22 @@ export interface TContact {
 export const contact: TContact[] = [
   {
     path: "https://github.com/PolinaKuksova2022",
-    img: "./github.svg",
+    img: "./static/github.svg",
     contact: "PolinaKuksova2022",
   },
   {
     path: "https://t.me/Polinak20",
-    img: "./tg.svg",
+    img: "./static/tg.svg",
     contact: "Polinak20",
   },
   {
     path: "https://wa.me/79995275091",
-    img: "./whatsapp.svg",
+    img: "./static/whatsapp.svg",
     contact: "79995275091",
   },
   {
     path: "mailto:pakuksova@gmail.com",
-    img: "./gmail.svg",
+    img: "./static/gmail.svg",
     contact: "pakuksova@gmail.com",
   },
 ];
