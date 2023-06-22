5fff57226bac675df3c67d36355d7a49a2570c89
Полина Куксова <pakuksova@gmail.com>
2023-06-22 21:30:08 +0300

    add new link

--- src/components/data/projects.ts
+++ src/components/data/projects.ts
@@ -28,7 +28,7 @@ export const projects: TProject[] = [
   },
   {
     path: "https://cheery-paletas-21c232.netlify.app/",
-    repo: "https://github.com/PolinaKuksova2022/CV-Kuksova-Polina/upload/gh-pages",
+    repo: "https://github.com/PolinaKuksova2022/Songbird.quiz",
     img: "./static/songbird.JPG",
     title: "Songbird",
     description:
